bfbe33c5bec4dfb36a7282ace111309d3a22bdd5
Abhishek2010DevSingh <abhishekkumar2010dev@gmail.com>
2025-03-05 11:39:41 +0530

    expense mirgration

--- migrations/20250305055803_expense_table.sql
+++ migrations/20250305055803_expense_table.sql
@@ -1,9 +1,16 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+CREATE TABLE expenses (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    description TEXT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+DROP TABLE expense;
 -- +goose StatementEnd
